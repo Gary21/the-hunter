52745859227435009747eb3829beb29ffa67e08b
Paweł Labudda <pawel.swift@gmail.com>
2023-04-20 21:56:49 +0200

    add health bar (#5)
    add player hp
    add hp bar

---
+++ The Hunter/Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    public int currentHealth = 100;
+    public int maxHealth = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
