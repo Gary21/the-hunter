2d4a0f011fb592eba46ba786eb5401414b353e74
turtlewneck <michal.wnek01@gmail.com>
2023-06-15 23:44:11 +0200

    add main menu
    Add main menu
    Connect main menu to the game

---
+++ The Hunter/Assets/Scripts/CalibrateDevice.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalibrateDevice : MonoBehaviour
+{
+    public bool isRunning = false;
+    public static float calibratedHeartRate = 0;
+
+    public void Calibrate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (isRunning)
+        {
+
+        }
+    }*/
+}
