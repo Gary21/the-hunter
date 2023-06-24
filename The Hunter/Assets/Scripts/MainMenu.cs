621684b925a7bce1d04f0ae8efdbe0d9c3f0b41f
Kawson <piegusek20@gmail.com>
2023-06-24 16:29:40 +0200

    Integrate device

--- The Hunter/Assets/Scripts/MainMenu.cs
+++ The Hunter/Assets/Scripts/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Settings()
+    {
+        SceneManager.LoadScene("SettingsUI");
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit!");
